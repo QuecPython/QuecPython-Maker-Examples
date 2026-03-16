08cd9d2d15ed582660416254da3b3273f8064060
陈志和 <aaron.chen@QUECTEL.COM>
2026-03-16 09:48:15 +0800

    demo

--- 整点报时器/RTC.py
+++ 整点报时器/RTC.py
@@ -5,10 +5,13 @@ from machine import RTC
 
 rtc = RTC()
 tts = audio.TTS(0)
-str1 = '大郎，该喝药了' 
+
 while True:
     data=rtc.datetime()
-    if data[5] == 0:
+    
+    if data[6] == 00:
+        print("当前时间：%04d-%02d-%02d %02d:%02d:%02d" % (data[0], data[1], data[2], data[4], data[5], data[6]))
+        str1 = "当前时间是：%04d年%02d月%02d日 %02d点%02d分%02d秒" % (data[0], data[1], data[2], data[4], data[5], data[6])
         tts.play(4,0,2,str1)
-    print("当前时间：%04d-%02d-%02d %02d:%02d:%02d" % (data[0], data[1], data[2], data[4], data[5], data[6]))
-    utime.sleep(10)    
+    
+    utime.sleep(1)    
